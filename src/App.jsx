f596ec96c441e7e8db6504be5f8aaca4d1a7ee37
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-10-05 19:04:49 -0300

    agruege navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Menu from "./components/common/Menu";
 import Admin from "./components/views/Admin";
 import Error404 from "./components/views/Error404";
 import Inicio from "./components/views/Inicio";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Inicio></Inicio>}></Route>
         <Route exact path="/administrador" element={<Admin></Admin>}/>
+        <Route exact path="/nav" element={<Menu></Menu>}/>
         <Route path="*" element={<Error404></Error404>}/>
       </Routes>
     </BrowserRouter>
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,9 +1,20 @@
+import {Container, Nav, Navbar } from "react-bootstrap";
+
 
 const Menu = () => {
     return (
-        <div>
-            <h2>Este es el navbar</h2>
-        </div>
+        <Navbar bg="danger"  expand="lg">
+        <Container>
+          <Navbar.Brand href="/"  className="text-white" >Cafeteria</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link  className="text-white" href="/">Inicio</Nav.Link>
+              <Nav.Link  className="text-white" href="/administrador">Admin</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
 };
 
